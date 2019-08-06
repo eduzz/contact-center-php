2a4e8d496f6374ff4af19be73ec5da843cde23ab
Diogo M Mainardes <diogo.mainardes@eduzz.com>
2019-08-06 13:11:08 -0300

    Acrescenta reports

--- src/Entities/GetDeliveriesEmailFilter.php
+++ src/Entities/GetDeliveriesEmailFilter.php
@@ -4,30 +4,22 @@ namespace Eduzz\ContactCenter\Entities;
 
 class GetDeliveriesEmailFilter extends GetDeliveriesFilter
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
+ public function __construct()
+ {
+  parent::__construct();
+ }
 
-    public function get()
-    {
-<<<<<<< HEAD
-        if (empty($this->filter['_metadata']))
-=======
-        if (empty($this->filter['_metadata'])) {
->>>>>>> 9cae77ab06c53d2e41181b430b0ab369a81c6c14
-            unset($this->filter['_metadata']);
-        }
+ public function get()
+ {
+  if (empty($this->filter['_metadata'])) {
+   unset($this->filter['_metadata']);
+  }
 
-<<<<<<< HEAD
-        if (empty($this->filter['date']))
-=======
-        if (empty($this->filter['date'])) {
->>>>>>> 9cae77ab06c53d2e41181b430b0ab369a81c6c14
-            unset($this->filter['date']);
-        }
+  if (empty($this->filter['date'])) {
+   unset($this->filter['date']);
+  }
 
-        return $this->filter;
-    }
+  return $this->filter;
+ }
 
 }
