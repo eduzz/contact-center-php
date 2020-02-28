2101830123bc68ed92dc72a714c2c6be21fa5c64
Diogo Mainardes <diogo.mainardes@eduzz.com>
2020-02-28 13:57:44 -0300

    Adiciona params no objeto pessoas

--- src/Entities/Person.php
+++ src/Entities/Person.php
@@ -6,11 +6,13 @@ class Person
 {
     private $name;
     private $email;
+    private $params;
 
-    public function __construct(string $email, string $name = null)
+    public function __construct(string $email, string $name = null, array $params = null)
     {
         $this->email = $email;
         $this->name  = $name;
+        $this->params = $params;
     }
 
     public function toArray()
@@ -18,6 +20,9 @@ class Person
         $data['email'] = $this->email;
         if ($this->name) {
             $data['name'] = $this->name;
+        }
+        if ($this->params) {
+            $data['params'] = $this->params;
         }
 
         return $data;
